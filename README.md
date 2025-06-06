caf3f2eda5a907da2a2436e0ef54e93949b274a1
Atrviet <viettorygh@gmail.com>
2025-06-06 19:41:14 +0700

    Update Logo

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Trang web mạng xã hội 
 Xây dựng một trang web mạng xã hội mini cho phép người dùng có thể đăng bài, chơi game, kết bạn, nhắn tin với nhau.
 
+git add .
+git commit -m "Update Logo"
+git push origin main
--- modules/posts/games/routes.py
+++ modules/posts/games/routes.py
@@ -22,3 +22,10 @@ def game2():
     # Truy vấn tất cả bạn bè (hoặc tất cả users trừ chính user)
     friends = User.query.filter(User.user_id != current_user.user_id).all()
     return render_template('posts/games/game_play_2.html', friends=friends)
+
+# Trò chơi 2: Rắn săn mồi
+@games_bp.route('/game3')
+def game3():
+    # Truy vấn tất cả bạn bè (hoặc tất cả users trừ chính user)
+    friends = User.query.filter(User.user_id != current_user.user_id).all()
+    return render_template('posts/games/game_play_3.html', friends=friends)
